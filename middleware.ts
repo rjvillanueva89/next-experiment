622fee8a9484e43c6d031d29813a62b0b5d76a9f
RJ Villanueva <rj.villanueva89@gmail.com>
2023-11-30 13:58:57 +0800

    fix: middleware authentication

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,27 @@
-export { default } from 'next-auth/middleware';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-export const config = { matcher: ['/dashboard'] };
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    console.log(request.nextUrl.pathname);
+    console.log(request.nextauth.token);
+
+    if (
+      request.nextUrl.pathname.startsWith('/dashboard') &&
+      !request.nextauth.token
+    ) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        // console.log(token);
+
+        return true;
+      },
+    },
+  },
+);
+
+// export const config = { matcher: ['/dashboard'] };

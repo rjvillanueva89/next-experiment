559cc6ba53b9b7370f4cb15ed1b9b0a4575fcaa0
RJ Villanueva <rj.villanueva89@gmail.com>
2023-11-30 12:23:50 +0800

    fix: next auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,51 @@
+import { authUser } from '@/app/lib/data';
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        identifier: {
+          label: 'Identifier',
+          type: 'text',
+          placeholder: 'Identifier',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      authorize: async (credentials) => {
+        const { identifier, password } = credentials || {};
+
+        const user = await authUser(identifier!, password!);
+
+        if (!user) return null;
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      return Promise.resolve(token);
+    },
+    session: async ({ session, token }) => {
+      if (token.sub) {
+        session.user.id = token.sub;
+      }
+
+      return Promise.resolve(session);
+    },
+  },
+  pages: {
+    signIn: '/login',
+    signOut: '/',
+  },
+};
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,66 +1,6 @@
-import { authUser } from '@/app/lib/data';
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { options } from './options';
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        identifier: {
-          label: 'Identifier',
-          type: 'text',
-          placeholder: 'Identifier',
-        },
-        password: {
-          label: 'Password',
-          type: 'password',
-          placeholder: 'Password',
-        },
-      },
-      authorize: async (credentials) => {
-        const { identifier, password } = credentials || {};
-
-        const user = await authUser(identifier!, password!);
-
-        if (!user) return null;
-
-        return user;
-      },
-    }),
-  ],
-  // callbacks: {
-  //   jwt: async ({ token, user }) => {
-  //     const userToken = {
-  //       ...token,
-  //       id: user.id,
-  //     };
-
-  //     console.log('userToken');
-  //     console.log(userToken);
-
-  //     return Promise.resolve(userToken);
-  //   },
-  //   session: async ({ session, token }) => {
-  //     const sessionResponse: Session = {
-  //       ...session,
-  //       user: {
-  //         id: token.sub!,
-  //         name: token.name!,
-  //         email: token.email!,
-  //       },
-  //     };
-
-  //     console.log('session');
-  //     console.log(sessionResponse);
-
-  //     return Promise.resolve(sessionResponse);
-  //   },
-  // },
-  pages: {
-    signIn: '/login',
-    signOut: '/',
-  },
-});
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };

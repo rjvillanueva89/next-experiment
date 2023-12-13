7ffa2271e8387dc5e4a19a225e94613fd090acc2
RJ Villanueva <rj.villanueva89@gmail.com>
2023-12-13 17:20:15 +0800

    refactor: todos

--- app/store/useTodosStore.ts
+++ app/store/useTodosStore.ts
@@ -1,11 +1,26 @@
 import { create } from 'zustand';
 
+type Todo = {
+  id: number;
+  description: string;
+  done: boolean;
+};
+
 type Store = {
-  todos: string[];
+  todos: Todo[];
   add: (todo: string) => void;
+  delete: (id: number) => void;
 };
 
 export const useTodosStore = create<Store>((set) => ({
   todos: [],
-  add: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
+  add: (todo) =>
+    set((state) => ({
+      todos: [
+        ...state.todos,
+        { id: Date.now(), description: todo, done: false },
+      ],
+    })),
+  delete: (id) =>
+    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
 }));
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -12,25 +12,37 @@ export default function TodosPage() {
     setValue('');
   };
 
+  console.log(todosStore.todos);
+
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div>
+      <div className="flex items-center gap-4 p-4">
+        <input
+          className="grow rounded-md border border-gray-200 p-4 text-sm outline-2 placeholder:text-gray-500"
+          id="name"
+          type="text"
+          placeholder="Enter your name"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <button onClick={handleClick}>add</button>
+      </div>
       <div>
-        <div className="flex items-center gap-4">
-          <input
-            className="grow rounded-md border border-gray-200 p-4 text-sm outline-2 placeholder:text-gray-500"
-            id="name"
-            type="text"
-            placeholder="Enter your name"
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          <button onClick={handleClick}>add</button>
-        </div>
-        <div>
-          {todosStore.todos.map((todo, key) => {
-            return <div key={key}>{todo}</div>;
-          })}
-        </div>
+        {todosStore.todos.map((todo, key) => {
+          return (
+            <div key={key} className="flex items-center gap-4 p-4">
+              <span className="grow">{todo.description}</span>
+              <button>complete</button>
+              <button
+                onClick={() => {
+                  todosStore.delete(todo.id);
+                }}
+              >
+                delete
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
